b7a3025012141ce9e67af79cd0ff14a06a313d68
Nonus <nonus@nonus.net>
2015-12-14 10:34:45 +0100

    Day 2 Done and dooooone!

---
+++ AdventOfCode/Day2/Present.cs
@@ -0,0 +1,53 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace Day2
+{
+    internal class Present
+    {
+        // ReSharper disable MemberCanBePrivate.Global
+        public int Length { get; }
+        public int Width { get; }
+        public int Height { get; }
+
+        public int TotalArea { get; private set; }
+        public int SmallestArea { get; private set; }
+
+        public int RibbonLength { get; private set; }
+        // ReSharper restore MemberCanBePrivate.Global
+
+        public Present(string dimensions)
+        {
+            var split = dimensions.Split('x');
+
+            Length = Convert.ToInt32(split[0]);
+            Width = Convert.ToInt32(split[1]);
+            Height = Convert.ToInt32(split[2]);
+
+            var sides = new List<int>
+                        {
+                            Length*Width,
+                            Width*Height,
+                            Height*Length
+                        };
+
+            sides.ForEach(x => TotalArea += 2*x);
+            SmallestArea = sides.Min();
+
+            var areasForRibbon = new List<int>
+                                 {
+                                     2*Length + 2*Width,
+                                     2*Width + 2*Height,
+                                     2*Height + 2*Length
+                                 };
+
+            var bowLength = Length*Width*Height;
+            RibbonLength = bowLength + areasForRibbon.Min();
+        }
+    }
+}
---
+++ AdventOfCode/Day2/Program.cs
@@ -0,0 +1,43 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+#endregion
+
+namespace Day2
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            string input;
+            Console.Write("Todays input:");
+
+            using (var fs = new StreamReader(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Inputs\Day2.txt")))
+                input = fs.ReadToEnd();
+
+            Debug.Assert(!string.IsNullOrWhiteSpace(input), "input != null nor whitespace");
+            Console.WriteLine(input);
+            Console.WriteLine("------- End of daily input -------");
+
+            var dimensions = input.Split('\n').ToList();
+            var presents = new List<Present>();
+            var totalPaper = 0;
+            var totalRibbon = 0;
+
+            // Create a list of new presents from out dimensions
+            dimensions.ForEach(x => presents.Add(new Present(x)));
+
+            // Since the presents themselves calculate all the areas we just need to call them
+            presents.ForEach(p => totalPaper += (p.SmallestArea + p.TotalArea));
+            presents.ForEach(p => totalRibbon += p.RibbonLength);
+
+            Console.WriteLine($"The elves need {totalPaper} square feet of wrapping paper and {totalRibbon} feet of ribbon.");
+            Console.ReadKey();
+        }
+    }
+}
